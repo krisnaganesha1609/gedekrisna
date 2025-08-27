fc00faa02df0981e7fe43ea770a437656d6ea033
krisnaganesha1609 <igkganes@gmail.com>
2025-08-27 13:38:43 +0700

    chore & fix: personalization portfolio

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -5,67 +5,72 @@ import { Card } from "../components/card";
 import { Navigation } from "../components/nav";
 import { BookmarkCheck, Medal, Trophy } from "lucide-react";
 const awards = [
-	{
-		icon: <Trophy size={20} />,
-		href: "",
-		desc: "3rd place, Lomba Kompetensi Siswa (LKS) is a competition held by Dinas Pendidikan Jawa Barat for Vocational High School. Hereby, I volunteered in Cyber Security chamber which a competition in defending/fortifiying server and network against attackers, and also attacking/exploitating at instructed server",
-		award: "LKS Cyber Security Tingkat Provinsi Jawa Barat Tahun 2022",
-	},
-	{
-		icon: <Trophy size={20} />,
-		href: "",
-		desc: "3rd place, Lomba Kompetensi Siswa (LKS) is a competition held by Dinas Pendidikan Jawa Barat for Vocational High School. Another fight but the results still the same :)",
-		award: "LKS Cyber Security Tingkat Provinsi Jawa Barat Tahun 2023",
-	},
-    {
-		icon: <Medal size={20} />,
-		href: "",
-		desc: "Best male students of 2023 graduation in Vocational High School 4 Bandung",
-		award: "Siswa Terbaik Putra SMKN 4 Bandung 2022-2023"
-	},
-	{
-		icon: <BookmarkCheck size={20} />,
-		href: "",
-		desc: "Approved Junior Mobile Programmer Certificate, issued by LSP Informatika at 2023",
-		award: "Junior Mobile Programmer Certificate"
-	}
+  {
+    icon: <Trophy size={20} />,
+    href: "",
+    desc: "Annual Year competition held by Dinas Pendidikan Jawa Barat for Vocational High School. Commonly doing on system hardening, CTF, and security stuff.",
+    award: "3rd place of 2022 LKS Cyber Security Jawa Barat",
+  },
+  {
+    icon: <Trophy size={20} />,
+    href: "",
+    desc: "Annual Year competition held by Dinas Pendidikan Jawa Barat for Vocational High School. Another fight but the results still the same :)",
+    award: "3rd place of 2023 LKS Cyber Security Jawa Barat",
+  },
+  {
+    icon: <Medal size={20} />,
+    href: "",
+    desc: "Best male students of 2023 graduation in Vocational High School 4 Bandung",
+    award: "Best Male Student",
+  },
+  {
+    icon: <Medal size={20} />,
+    href: "",
+    desc: "Capture The Flag Competition held by Telkom University Informatics Faculty. Jeopardy Style CTF featuring Forensics, Cryptography, Web Exploit, Binary Exploit, and Reverse Engineering",
+    award: "2nd Best Write Up CTF ADIKARA 2024",
+  },
+  {
+    icon: <BookmarkCheck size={20} />,
+    href: "",
+    desc: "Approved Junior Mobile Programmer Certificate, issued by LSP Informatika at 2023",
+    award: "Junior Mobile Programmer Certificate",
+  },
 ];
 export default function Awards() {
-    return (
-        <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-            <Navigation />
-            <div className="container flex items-center justify-center min-h-screen px-4 mx-auto pt-20">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-					{awards.map((s) => (
-						<div key={s.award}>
-						<Card>
-							<Link
-								href={s.href}
-								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-							>
-								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
-								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-									{s.icon}
-								</span>{" "}
-								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display text-center">
-										{s.award}
-									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-										{s.desc}
-									</span>
-								</div>
-							</Link>
-						</Card>
-						</div>
-						
-					))}
-				</div>
-			</div>
+  return (
+    <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <Navigation />
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto pt-20">
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+          {awards.map((s) => (
+            <div key={s.award}>
+              <Card>
+                <Link
+                  href={s.href}
+                  target="_blank"
+                  className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                >
+                  <span
+                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                    aria-hidden="true"
+                  />
+                  <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                    {s.icon}
+                  </span>{" "}
+                  <div className="z-10 flex flex-col items-center">
+                    <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display text-center">
+                      {s.award}
+                    </span>
+                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                      {s.desc}
+                    </span>
+                  </div>
+                </Link>
+              </Card>
+            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import Image from "next/image";
 
 const navigation = [
-  {name: "Skills", href:"/skills"},
+  { name: "Skills", href: "/skills" },
   { name: "Projects", href: "/projects" },
-  { name: "Awards", href: "/awards"},
+  { name: "Awards", href: "/awards" },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -26,12 +25,12 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      
+
       <h1 className="z-10 text-4xl text-transparent duration-1000 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
         <span className="bg-[linear-gradient(to_right,#0F0F0F,#232D3F,#EAD7BB,#FFF2D8)] bg-300% bg-min100% animate-names text-transparent bg-clip-text">
           Gede Krisna
@@ -41,22 +40,31 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I&apos;m an informatics student, software engineer, junior mobile dev, and junior ethical hacker.     
+          I&apos;m an informatics student, junior mobile developer, and{" "}
+          <Link href="/flagsub" className="text-zinc-400 cursor-text">
+            CTF
+          </Link>{" "}
+          player.
         </h2>
+
         <h2 className="text-sm text-zinc-500 ">
-          Beginner designer, Intermediate coder.
+          Ready to{" "}
+          <Link
+            href="/contact"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            connect and discuss!
+          </Link>
         </h2>
-        <h2 className="text-sm text-zinc-500 ">
-          Ready to{" "} 
-          <Link href="/contact" className="underline duration-500 hover:text-zinc-300">connect and discuss!</Link>
-          
+        <h2 className="text-xs text-zinc-700 mt-6">
+          Something might be hidden here... if you know what I mean
+        </h2>
+        <h2 className="text-sm text-zinc-500 mt-6 hidden">
+          Flag Check: SCATTER&#123;w0nd3r_wh47_7h15_d035??_W47CH_7H3M_RUN!&#125;
         </h2>
       </div>
-      
-      
     </div>
   );
-
 }
 
 // #0F0F0F,#232D3F,#005B41,#008170
